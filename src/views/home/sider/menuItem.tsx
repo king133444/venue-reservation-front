612cab5c42377887b2c74b6654e0c2d07cd8f673
cxx0505 <381142773@qq.com>
2024-09-18 13:15:50 +0800

    fix: 修复菜单栏bug

--- src/views/home/sider/menuItem.tsx
+++ src/views/home/sider/menuItem.tsx
@@ -22,31 +22,14 @@ function getItem(
 }
 
 export default function getItems() {
-	const role = sessionStorage.getItem('roleId');
 	let items: ItemType[] = [];
 
-	if (role === '1' || role === '2') {
-		items = [
-			getItem('预约管理', 'menu1', <HomeOutlined rev={undefined} />),
-			getItem('场馆动态', 'menu2', <ProjectOutlined rev={undefined} />),
-			getItem('人员管理', 'menu3', <SmileOutlined rev={undefined} />),
-			getItem('退出', 'menu4', <LogoutOutlined rev={undefined} />)
-		];
-	} else if (role === '3' || role === '4' || role === '5' || role === '6') {
-		items = [
-			getItem('预约管理', 'menu1', <HomeOutlined rev={undefined} />),
-			getItem('场馆动态', 'menu2', <ProjectOutlined rev={undefined} />),
-			getItem('人员管理', 'menu3', <SmileOutlined rev={undefined} />),
-			getItem('退出', 'menu4', <LogoutOutlined rev={undefined} />)
-		];
-	} else if (role === '7') {
-		items = [
-			getItem('预约管理', 'menu1', <HomeOutlined rev={undefined} />),
-			getItem('场馆动态', 'menu2', <ProjectOutlined rev={undefined} />),
-			getItem('人员管理', 'menu3', <SmileOutlined rev={undefined} />),
-			getItem('退出', 'menu4', <LogoutOutlined rev={undefined} />)
-		];
-	}
+	items = [
+		getItem('预约管理', 'menu1', <HomeOutlined rev={undefined} />),
+		getItem('场馆动态', 'menu2', <ProjectOutlined rev={undefined} />),
+		getItem('人员管理', 'menu3', <SmileOutlined rev={undefined} />),
+		getItem('退出', 'menu4', <LogoutOutlined rev={undefined} />)
+	];
 
 	return items;
 }
